c6b517e92e86cd5c1d4084fc2977f9bcf33177cc
박승호 <p_vatzz@naver.com>
2023-10-10 23:28:55 +0900

    Add CoreData

---
+++ Github-Contribution/Data/Storage/CoreDataStorage.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataStorage.swift
+//  Github-Contribution
+//
+//  Created by 박승호 on 10/10/23.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataStorage {
+    static let shared = CoreDataStorage()
+    private init() {  }
+    
+    private lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataStorage")
+        container.loadPersistentStores { description, error in
+            if let error = error as? NSError {
+                assertionFailure("CoreDataStorage Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+    
+    func saveContext() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                assertionFailure("CoreDataStorage Unresolved error \(error), \((error as NSError).userInfo)")
+            }
+        }
+    }
+}
