e8d6513bca86121e711dd9a0276028d2d553f042
박승호 <p_vatzz@naver.com>
2023-10-11 01:11:32 +0900

    Commit

---
+++ Github-Contribution/Data/Network/APIEndPoints.swift
@@ -0,0 +1,14 @@
+//
+//  APIEndPoints.swift
+//  Github-Contribution
+//
+//  Created by 박승호 on 10/11/23.
+//
+
+import Foundation
+
+struct APIEndPoints {
+    static func getProfile(type: VCSType, userName: String) {
+        
+    }
+}
