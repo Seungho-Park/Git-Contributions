f1b4173c00657e23da3b9ea27526adeaaf3c2fa3
박승호 <p_vatzz@naver.com>
2025-01-09 12:31:12 +0900

    Initial Commit

---
+++ Workspace.swift
@@ -0,0 +1,15 @@
+//
+//  Workspace.swift
+//  Manifests
+//
+//  Created by 박승호 on 1/9/25.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "Git-Contributions",
+    projects: [
+        "Projects/Application"
+    ]
+)
