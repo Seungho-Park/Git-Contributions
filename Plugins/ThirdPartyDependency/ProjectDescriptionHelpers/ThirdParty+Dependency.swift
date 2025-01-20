0e813bd8753113149da398bb280e5c2b9b01c539
박승호 <p_vatzz@naver.com>
2025-01-21 03:10:25 +0900

    # ThirdPartyLibs 의존성 분리 (Rx -> SharedThirdPartyLibs)

--- Plugins/ThirdPartyDependency/ProjectDescriptionHelpers/ThirdParty+Dependency.swift
+++ Plugins/ThirdPartyDependency/ProjectDescriptionHelpers/ThirdParty+Dependency.swift
@@ -11,6 +11,7 @@ public extension ProjectDescription.TargetDependency {
     enum SPM {
         public static let RxSwift: ProjectDescription.TargetDependency = .external(name: "RxSwift")
         public static let RxCocoa: ProjectDescription.TargetDependency = .external(name: "RxCocoa")
+        public static let RxRelay: ProjectDescription.TargetDependency = .external(name: "RxRelay")
         public static let FlexLayout: ProjectDescription.TargetDependency = .external(name: "FlexLayout")
         public static let PinLayout: ProjectDescription.TargetDependency = .external(name: "PinLayout")
     }
