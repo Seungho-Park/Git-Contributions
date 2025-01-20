561a4147da15272ab9815fb2730c272518158361
박승호 <p_vatzz@naver.com>
2025-01-20 22:10:36 +0900

    오타 수정

--- Projects/Domain/User/Sources/UserRepositoryImpl.swift
+++ Projects/Domain/User/Sources/UserRepositoryImpl.swift
@@ -5,7 +5,7 @@
 //  Created by 박승호 on 1/20/25.
 //  Copyright © 2025 Seungho-Park. All rights reserved.
 //
-import SharedStorageInterface
+import CoreStorageInterface
 import SharedStorage
 import CoreData
 import DomainUserInterface
--- Projects/Domain/User/Testing/Sources/MockUserRepository.swift
+++ Projects/Domain/User/Testing/Sources/MockUserRepository.swift
@@ -6,9 +6,16 @@
 //  Copyright © 2025 Seungho-Park. All rights reserved.
 //
 
+import CoreStorageInterface
 import DomainUserInterface
 
 public final class MockUserRepository: UserRepository {
+    private let storage: CoreDataStorage
+    
+    public init(storage: CoreDataStorage) {
+        self.storage = storage
+    }
+    
     public func fetchUserList(completion: @escaping (Result<[User], any Error>) -> Void) {
         
     }
